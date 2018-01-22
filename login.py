d8430c42db0fd8de552fea087699b3782503a2a4
DBlackKat <zhirui09400@gmail.com>
2018-01-22 11:17:06 +0800

    change from web request to selenium

--- login.py
+++ login.py
@@ -1,64 +1,34 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 import argparse
-import base64
-import json
-import requests
-
-
-VERBOSE = False
-
-def log(msg, *args, **kwargs):
-    if VERBOSE:
-        print(msg, *args, **kwargs)
-
-
+chromeOptions = webdriver.ChromeOptions()
+# prefs = {"profile.managed_default_content_settings.images": 2}
+# chromeOptions.add_experimental_option("prefs", prefs)
+chromeOptions.add_argument("--headless")
 def parse_args():
-    global VERBOSE
     parser = argparse.ArgumentParser()
-    parser.add_argument('--username', '-u')
-    parser.add_argument('--password', '-p')
-    parser.add_argument('--verbose', '-v', action='store_true')
-    parser.add_argument('--login', action='store_true')
-    parser.add_argument('--logout', action='store_true')
+    parser.add_argument('--username','-u')
+    parser.add_argument('--password','-p')
     args = parser.parse_args()
-    VERBOSE = bool(args.verbose)
     return args
 
-def need_login():
-    log('Testing network...')
-    r = requests.get('http://www.nctu.edu.tw')
-    if r.text.find('NCTU Dormitory Network') != -1:
-        return True
-    return False
 
-def login(args):
-    log('login as', args.username)
-    payload = {
-        'username': args.username,
-        'password': base64.b64encode(args.password.encode())
-    }
-    r = requests.post('http://www.nctu.edu.tw/auth/', data=payload)
-    body = json.loads(r.json()['body'])
-    if r.status_code == 200 and body['status'] == 'Enable':
-        return True
-    else:
-        return False
+def login(username_fill,password_fill):
+    driver = webdriver.Chrome('misc/selenium/chromedriver',chrome_options=chromeOptions)
 
-def logout():
-    log('logout')
-    r = requests.post('http://140.113.178.244/unauth/', data={})
-    body = json.loads(r.json()['body'])
-    if r.status_code == 200 and body['status'] == 'Disable':
-        return True
-    else:
-        return False
+    driver.get("http://140.113.0.38/smsauth/12/pc.php?declare=true&params=pwd&orgi=http://www.nctu.edu.tw/auth/")
+    username = driver.find_element_by_name("username")
+    password = driver.find_element_by_name("userpwd")
+    username.send_keys(username_fill)
+    password.send_keys(password_fill)
+    driver.find_element_by_name("btlogin").click()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_args()
-    if args.login:
-        if need_login():
-            login(args)
-    elif args.logout:
-        logout()
+    login(args.username,args.password)
+
 
